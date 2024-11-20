482a13243f5682f7d27719da1aa1cb3098baacad
Rajesh Haldar <rhrajeshaldar@gmail.com>
2024-11-21 01:54:43 +0530

    for each loop

---
+++ loop/foreach/sample/Sample.java
@@ -0,0 +1,12 @@
+package foreach.sample;
+
+public class Sample {
+	public static void main(String[] args) {
+		int[] arg = {3,4,2,6,7};
+		
+		for(int i : arg) {
+			System.out.println(i);
+		}
+	}
+
+}
